2950a4d5d48f0c35c54be07579e46f9e6983d70a
Ayush Goyal <ayush987goyal@gmail.com>
2018-01-18 22:58:31 +0530

    added cordova plugin for storage and inital service and pipeline for notes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AddNotePage } from '../add-note/add-note';
+import { NoteService } from './../../providers/note-service/note-service';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  notes: { title: string }[] = [];
 
+  constructor(public navCtrl: NavController,
+    private noteService: NoteService) {
+
+  }
+
+  ionViewWillEnter(){
+    this.notes = this.getAllNotes();
+  }
+
+  addNote() {
+    this.navCtrl.push(AddNotePage);
+  }
+
+  getAllNotes() {
+    return this.noteService.getAllNotes();
   }
 
 }
